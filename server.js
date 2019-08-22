dbfadd3da8a3de5bcabe1fb39ca3082229e3aa49
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-08-22 15:06:23 +0200

    Add GET endpint

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ var app = express();
 app.use(express.static('assets'));
 
 app.get('/', function (req, res) {
-    res.send('Hello world');
+    res.sendFile('/index.html');
+});
+
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.json(response);
 });
 
 var server = app.listen(3000, 'localhost', function() {
